9d4dacbd2dafd000d8b6afb4d29d379bb534b3c4
Eason <1547837791@qq.com>
2023-11-20 23:56:06 +0800

    08days

---
+++ 08days-Form-Wave/script.js
@@ -0,0 +1,5 @@
+const labels = document.querySelectorAll('.form_container label')
+
+labels.forEach(label => {
+    label.innerHTML = label.innerHTML.split('').map((letter,idx) => `<span style = "transition-delay:${idx*50}ms">${letter}</span>`).join('')
+})
